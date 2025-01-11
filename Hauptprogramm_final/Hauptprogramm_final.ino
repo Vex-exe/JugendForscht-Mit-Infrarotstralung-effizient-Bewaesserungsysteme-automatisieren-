3e848dbdf7d027d6bca853f2765ccf329214d854
Vex-exe <exoticbutter313@gmail.com>
2025-01-11 10:35:30 +0100

    Update Hauptprogramm_final.ino
    More exact comments

--- Hauptprogramm_final/Hauptprogramm_final.ino
+++ Hauptprogramm_final/Hauptprogramm_final.ino
@@ -3,7 +3,7 @@
 #define PI 3.1415926535897932384626433832795
   
   float NDMI; //Endziel, das ermittelt werden soll
-  float minNDMI = -0.95;
+  float minNDMI = -0.95;  //Minimal erlaubter NDMI-Wert, der zugelassen ist
   float Wert830;  //Wert der 830nm-Diode
   float Wert1600; //Wert der 1600-Diode
  
@@ -11,19 +11,19 @@
   int StepsPerRev = 400;  //Schritte pro 360-Grad-Umdrehung
   float GradProStep = 0.9;  //Winkelveränderung pro Schrizz
 
-  int Z_SP = 4;
+  int Z_SP = 4; //Pins für den Z-Motor
   int Z_DP = 5;
   float Z_Rotation = 0; //Rotation auf der Z-Achse
   bool R_Rotation = true; //Dreht sich der Stepper nach rechts?
 
-  int Y_SP = 3;
+  int Y_SP = 3; //Pins für den Y-Motor
   int Y_DP = 2;
   const float Y_maxRotation = 60; //Maximale Rotation auf der Y-Achse
   float Y_Rotation = 60;  //Rotation auf der Y-Achse
   float Y_minRotation = 45;  //Minimale Rotation auf der Y-Achse
   bool Y_finished = false;  //Wurde die Rotation auf der Y-Achse vollständig durchgeführt?
 
-  int A_SP = 6;
+  int A_SP = 6; //Pins für den A-Motor
   int A_DP = 7;
 
   //Trigonometrie-Variablen
@@ -36,6 +36,7 @@
   bool turnON = false;  //Soll die Messung laufen?
   String msg; //Signal von Python
 
+  //Pins für die Servo-Motoren
   int Serv_Pin1 = 8;
   int Serv_Pin2 = 9;
   int Serv_Pin3 = 10;
@@ -46,6 +47,7 @@
   Stepper StepperY = Stepper(StepsPerRev, Y_DP, Y_SP);
   Stepper StepperA = Stepper(StepsPerRev, A_DP, A_SP);
 
+  //Servo-Motoren
   Servo Servo1;
   Servo Servo2;
   Servo Servo3;
@@ -53,11 +55,11 @@
 
 void setup() {
   Serial.begin(9600);
-  StepperZ.setSpeed(500);
+  StepperZ.setSpeed(500); //Legt die Geschwindigkeit der Stepper-Motoren fest
   StepperY.setSpeed(500);
   StepperA.setSpeed(500);
 
-  Servo1.attach(Serv_Pin1);
+  Servo1.attach(Serv_Pin1); //Weißt den Servo-Motoren ihre Pins zu
   Servo2.attach(Serv_Pin2);
   Servo3.attach(Serv_Pin3);
   Servo4.attach(Serv_Pin4);
@@ -72,7 +74,7 @@ void loop()
       }
       else if(msg == "OFF"){  //Ist das Signal "OFF", dann...
         turnON = false; //Stoppe die Messung
-        reset();
+        reset();  //Setze die Motoren in ihre Ursprungsposition zurück
       }
     }
     
@@ -81,7 +83,7 @@ void loop()
         trigon(); //Berechne die Koordinaten des beobachteten Punktes
         calculate();  //Berechne den NDMI
         send(); //Sende die Werte an Python
-        automate();
+        automate(); //Bewässere Stellen, deren NDMI zu niedrig ist
         delay(500);
     }
 }
@@ -142,19 +144,19 @@ void send(){
 }
 
 void reset(){
-  StepperZ.step((Z_Rotation/StepsPerRev)*-4);
-  StepperY.step((Y_maxRotation - Y_Rotation/GradProStep) *-4);
-  Y_Rotation = Y_maxRotation;
-  Z_Rotation = 0; 
+  StepperZ.step((Z_Rotation/StepsPerRev)*-4); //Lasse den Z-Motor zurück an seine Ursprungsposition laufen
+  StepperY.step((Y_maxRotation - Y_Rotation/GradProStep) *-4);  //Lasse den Y-Motor zurück an seine Ursprungsposition laufen
+  Y_Rotation = Y_maxRotation; //Setze die Y-Rotation zurück 
+  Z_Rotation = 0; //Setze die Z-Rotation zurück 
 }
 
 void automate(){
-  if(NDMI < minNDMI){
-    StepperA.step((Z_Rotation/StepsPerRev)*4);
-    if(Messradius <= 1.18){
-      Servo1.write(90);
-      delay(2000);
-      Servo1.write(-90);
+  if(NDMI < minNDMI){ //Ist der NDMI unter dem mindestens erlaubten Wert, dann...
+    StepperA.step((Z_Rotation/StepsPerRev)*4);  //Rotiere StepperA zur aktuellen Z-Rotation
+    if(Messradius <= 1.18){ //Prüfe, in welchem Abstand sich der Punkt befindet und führe die jeweilige if-Schleife aus
+      Servo1.write(90); //Öffne das zugehörige Ventil...
+      delay(2000);  //...für zwei Sekunden...
+      Servo1.write(-90);  //und schließe es wieder
     }
     else if(Messradius <= 1.36 && Messradius > 1.18){
       Servo2.write(90);
@@ -171,7 +173,7 @@ void automate(){
       delay(2000);
       Servo4.write(-90);
     }
-    StepperA.step((Z_Rotation/StepsPerRev)*-4);
+    StepperA.step((Z_Rotation/StepsPerRev)*-4); //Rotiere StepperA zurück in seine Ausgangsposition
     delay(1000);
   }
 }
